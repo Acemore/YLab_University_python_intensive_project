775e4fd60a66368d7c499fd7517151b4942b8eb2
Sergey Martynov <acemore@mail.ru>
2023-08-20 14:32:52 +0300

    Refactor extracting data from env-file

--- menu_app/database.py
+++ menu_app/database.py
@@ -7,7 +7,11 @@ from sqlalchemy.orm import sessionmaker
 
 dotenv.load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL')
+POSTGRES_HOST = os.getenv('POSTGRES_HOST')
+POSTGRES_USER = os.getenv('POSTGRES_USER')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+POSTGRES_DB = os.getenv('POSTGRES_DB')
+SQLALCHEMY_DATABASE_URL = f'postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:5432/{POSTGRES_DB}'  # noqa: E501
 
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 async_session = sessionmaker(
